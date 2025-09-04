9c7a97d59be01791d7a2a374db503e28fee503d8
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-09-04 19:49:54 +0800

    feat: Finish make component topbar

--- src/components/Organisms/Navbar.tsx
+++ src/components/Organisms/Navbar.tsx
@@ -3,27 +3,51 @@
 import Link from "next/link";
 import Image from "next/image";
 import {usePathname} from "next/navigation";
+import Topbar from "@/components/Organisms/Topbar";
+import {useState} from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import {bounceTransition} from "@/utils/motion-variant";
 
 export default function Navbar() {
     const pathname = usePathname();
+    const [isPopupCategoryOpen, setIsPopupCategoryOpen] = useState<boolean>(false);
+
+    const togglePopupCategory = () => {
+        setIsPopupCategoryOpen(!isPopupCategoryOpen);
+    }
 
     return (
-        <nav className="navbar">
-            <div className="container">
-                <Link href="/" className="navbar-brand">
-                    <Image src="/image/brand/brand-logo.svg" alt="Brand Logo" width={160} height={52}/>
-                </Link>
-                <div className="navbar-menu">
-                    <Link href="/" className={`menu-item ${pathname === '/' ? 'active' : ''}`}>Beranda</Link>
-                    <Link href="/about" className={`menu-item ${pathname === '/about' ? 'active' : ''}`}>Tentang Kami</Link>
-                    <button type="button" className={`menu-item ${pathname.startsWith('/culture') ? 'active' : ''}`}>Kategori</button>
-                    <Link href="/collection" className={`menu-item ${pathname.startsWith('/collection') ? 'active' : ''}`}>Koleksi 3D</Link>
+        <>
+            <nav className="navbar">
+                <div className="container">
+                    <Link href="/" className="navbar-brand">
+                        <Image src="/image/brand/brand-logo.svg" alt="Brand Logo" width={160} height={52}/>
+                    </Link>
+                    <div className="navbar-menu">
+                        <Link href="/" className={`menu-item ${pathname === '/' ? 'active' : ''}`} onClick={() => setIsPopupCategoryOpen(false)}>Beranda</Link>
+                        <Link href="/about" className={`menu-item ${pathname === '/about' ? 'active' : ''}`} onClick={() => setIsPopupCategoryOpen(false)}>Tentang</Link>
+                        <button type="button" className={`menu-item ${pathname.startsWith('/culture') ? 'active' : ''}`} onClick={togglePopupCategory}>Kategori</button>
+                        <Link href="/collection" className={`menu-item ${pathname.startsWith('/collection') ? 'active' : ''}`} onClick={() => setIsPopupCategoryOpen(false)}>Koleksi</Link>
+                    </div>
+                    <Link href="/chat" className="navbar-button w-fit">
+                        Tanya AI
+                        <Image src="/image/icon/chat/chat-light.svg" alt="Chat Icon" width={16} height={16}/>
+                    </Link>
                 </div>
-                <Link href="/chat" className="navbar-button w-fit">
-                    Tanya AI
-                    <Image src="/image/icon/chat/chat-light.svg" alt="Chat Icon" width={16} height={16}/>
-                </Link>
-            </div>
-        </nav>
+            </nav>
+            <AnimatePresence>
+                {isPopupCategoryOpen && (
+                    <motion.div
+                        className="topbar-container container"
+                        initial={{ y: 10, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: 10, opacity: 0 }}
+                        transition={bounceTransition}
+                    >
+                        <Topbar onClickAction={() => setIsPopupCategoryOpen(false)} />
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </>
     )
 }
--- src/components/Organisms/Topbar.tsx
+++ src/components/Organisms/Topbar.tsx
@@ -1,9 +1,60 @@
 import Link from "next/link";
 
-export default function Topbar() {
+type Props = {
+    onClickAction: () => void;
+}
+
+export default function Topbar({onClickAction}: Props) {
     return (
         <div className="topbar">
-            <Link href="/"></Link>
+            <Link href="/culture/destinasi-budaya" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/mountain/mountain-primary.svg')] group-hover/link:bg-[url('/image/icon/mountain/mountain-light.svg')]"></span>
+                </div>
+                Destinasi Budaya
+            </Link>
+            <Link href="/culture/adat-tradisi" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/food/food-primary.svg')] group-hover/link:bg-[url('/image/icon/food/food-light.svg')]"></span>
+                </div>
+                Adat & Tradisi
+            </Link>
+            <Link href="/culture/makanan-kuliner" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/mask/mask-primary.svg')] group-hover/link:bg-[url('/image/icon/mask/mask-light.svg')]"></span>
+                </div>
+                Makanan & Kuliner
+            </Link>
+            <Link href="/culture/busana-aksesoris" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/art/art-primary.svg')] group-hover/link:bg-[url('/image/icon/art/art-light.svg')]"></span>
+                </div>
+                Busana & Aksesoris
+            </Link>
+            <Link href="/culture/seni-pertunjukan" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/prayer/prayer-primary.svg')] group-hover/link:bg-[url('/image/icon/prayer/prayer-light.svg')]"></span>
+                </div>
+                Seni Pertunjukan
+            </Link>
+            <Link href="/culture/bahasa-sastra" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/cloth/cloth-primary.svg')] group-hover/link:bg-[url('/image/icon/cloth/cloth-light.svg')]"></span>
+                </div>
+                Bahasa & Sastra
+            </Link>
+            <Link href="/culture/seni-rupa-kerajinan" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/pen/pen-primary.svg')] group-hover/link:bg-[url('/image/icon/pen/pen-light.svg')]"></span>
+                </div>
+                Seni Rupa & Kerajinan
+            </Link>
+            <Link href="/culture/arsitektur-bangunan" className="topbar-link group/link" onClick={onClickAction}>
+                <div className="link-icon group-hover/link:bg-primary">
+                    <span className="icon w-[12px] h-[12px] bg-[url('/image/icon/home/home-primary.svg')] group-hover/link:bg-[url('/image/icon/home/home-light.svg')]"></span>
+                </div>
+                Arsitektur & Bangunan
+            </Link>
         </div>
     );
 }
