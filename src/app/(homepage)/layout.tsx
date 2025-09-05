a3c86e243e27d5865505dbf2c1d352e5d5c15ce8
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-09-05 13:52:09 +0800

    fix: Try to fix favicon show on safari

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -21,6 +21,23 @@ export const metadata: Metadata = {
         locale: "id_ID",
         type: "website",
     },
+    icons: {
+        icon: [
+            { url: "/image/favicon/favicon.ico" },
+            { url: "/image/favicon/favicon.svg", type: "image/svg+xml" },
+            { url: "/image/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            { url: "/image/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+            { url: "/image/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/image/favicon/apple-touch-icon.png", sizes: "180x180" },
+        ],
+        shortcut: "/image/favicon/favicon.ico",
+    },
+    other: {
+        "apple-mobile-web-app-title": "Adiwidia",
+    },
+    manifest: "/image/favicon/manifest.json",
 };
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
